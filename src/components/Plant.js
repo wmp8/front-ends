a00b7750490c16022b64e23c7c6233d19885c63a
Krystle Mitchell <mitchell.krystle2@gmail.com>
2021-11-16 13:06:15 -0800

    Create header

--- src/components/Plant.js
+++ src/components/Plant.js
@@ -1,1 +1,13 @@
 // plant has id: integer ,nickname: string  ,species: string h2o frequency:Type determined by implementation, image
+
+import React from "react"
+
+const Plant = () => {
+    return (
+        <div>
+            <h1>Plant</h1>
+            
+            </div>
+
+    )
+}
