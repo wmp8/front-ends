074de8b63af0ffd1c99343f8f092fa495479d13f
Krystle Mitchell <mitchell.krystle2@gmail.com>
2021-11-16 00:42:26 -0800

    added hamburger menu, and learning Bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,39 @@
 // home signup, login/logout, addPlant, update personal
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { Navbar } from "react-bootstrap";
+import * as ReactBootStrap from "react-bootstrap";
+
+const NavBar = () => {
+  return (
+    <div class="header">
+      <nav class="navbar navbar-expand-sm navbar-light bg-light">
+        <button class="navbar-brand">Home</button>
+        <button class="navbar-toggler" data-toggle="collapse" data-target='#navBarMenu' >
+
+            <span class='navbar-toggler-icon'></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navBarMenu">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <button class="nav-link"> Signup </button>
+          </li>
+          <li class="nav-item">
+            <button class="nav-link">
+             Log-In
+            </button>
+          </li>
+        </ul>
+
+        </div>
+        
+
+       
+
+        
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
