10b8c167e4231e2bcfcae210f139ee542b25b33f
Krystle Mitchell <mitchell.krystle2@gmail.com>
2021-11-17 23:28:33 -0800

    CSS changes to Plants

--- src/components/Plant.js
+++ src/components/Plant.js
@@ -18,47 +18,59 @@ console.log(plantData);
 const Plant = () => {
   const [data, setData] = useState(plantData);
 
-  axios.get('https://github.com/wmp8/back-ends#get-apiplantsplant_id')
-  .then((res) => {
-      
-  })
-.catch((err) => {
-
-});
+  axios
+    .get("https://github.com/wmp8/back-ends#get-apiplantsplant_id")
+    .then((res) => {})
+    .catch((err) => {});
 
   return (
     <div className="plant-container">
       <h1>Plant Diaries</h1>
 
       <div className="plant-card">
-        <h3>Name: {data.nickname}</h3>
+        <div className="topPlant">
+          
+          <h3>Name: {data.nickname}</h3>
 
-        <div className="img-container">
-          <img className="plantImg" src={ data.image_url } alt={data.nickname}/>
+          <div className="img-container">
+            <img
+              className="plantImg"
+              src={data.image_url}
+              alt={data.nickname}
+            />
+          </div>
         </div>
-        
+
         <div className="plant-card-content">
           <div className="text">
-            <p> Light Requirement: { data.light_requirement }</p>
-            <p>
-                Plant ID: {data.plant_id}
-            </p>
+            <div className="plantText">
+              <p> Light Requirement: {data.light_requirement}</p>
+            </div>
 
-            <p>
-                Species: {data.species}
-            </p>
-            <p>User-ID: {data.user_id}</p>
-            <p>Water frequency: {data.water_frequency}</p>
+            <div className="plantText">
+              <p>Plant ID: {data.plant_id}</p>
+            </div>
+
+            <div className="plantText">
+              <p>Species: {data.species}</p>
+            </div>
+
+            <div className="plantText">
+              <p>User-ID: {data.user_id}</p>
+            </div>
+
+            <div className="plantText">
+              <div className="water">
+                <p>Water frequency: {data.water_frequency}</p>
+              </div>
+            </div>
           </div>
 
           <button>Edit</button>
-          
         </div>
-        
       </div>
     </div>
   );
-
 };
 
-export default Plant
+export default Plant;
